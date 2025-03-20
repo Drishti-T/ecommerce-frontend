5162185e3fbc706c53c1340aa12458d7e901e529
root <tanwardrishti20@gmail.com>
2025-03-21 00:03:27 +0530

    updated herosection

--- src/Components/BasicButton.jsx
+++ src/Components/BasicButton.jsx
@@ -1,4 +1,4 @@
-export const BasicButton = ({bg , textcolor}) => {
+export const BasicButton = ({bg , textcolor , px , py , w , h}) => {
     return <div>
       <button className={`${bg} ${textcolor} rounded-full py-4 px-20`}>Shop Now</button>
     </div>
--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -6,27 +6,27 @@ export const HeroSection = () => {
 
             {/* left */}
             <div className="w-full flex flex-col">
-                <div className="font-integral_cf  text-4xl sm:text-6xl">FIND CLOTHES <br></br>THAT MATCHES <br></br>YOUR STYLE </div>
+                <div className="font-integral_cf text-4xl sm:text-6xl">FIND CLOTHES <br></br>THAT MATCHES <br></br>YOUR STYLE </div>
 
-                <div className="text-gray-500 text-sm py-8">Browse through our diverse range of meticulously crafted garments, designes to bring out your individuality and cater to your sense of style </div>
+                <div className="text-gray-500 sm:text-lg py-8">Browse through our diverse range of meticulously crafted garments, designed <br /> to bring out your individuality and cater to your sense of style </div>
 
                 <div><BasicButton bg={"bg-black"} textcolor={"text-white"}></BasicButton></div>
 
-                <div className="pt-16 flex flex-wrap justify-around sm:flex-nowrap sm:justify-normal gap-10">
+                <div className="pt-16 flex flex-wrap justify-center sm:justify-around sm:flex-nowrap sm:justify-normal gap-5 sm:gap-10">
 
-                    <div className="flex flex-col pr-10 border-r-2 border-gray-300">
-                        <div className=" font-satoshi_bold text-5xl">200+</div>
-                        <div className="text-gray-500">International Brands</div>
+                    <div className="flex flex-col pr-5 sm:pr-10 border-r-2 border-gray-300">
+                        <div className="font-satoshi_bold text-2xl sm:text-5xl">200+</div>
+                        <div className="text-gray-500 text-xs sm:text-sm">International Brands</div>
                     </div>
 
-                    <div className="flex flex-col pr-10 border-r-2 border-gray-300">
-                        <div className=" text-5xl font-satoshi_bold">2,000+</div>
-                        <div className="text-gray-500">High-Quality Products</div>
+                    <div className=" flex flex-col  sm:pr-10 sm:border-r-2 sm:border-gray-300">
+                        <div className="text-2xl sm:bg-green-400 sm:text-5xl font-satoshi_bold">2,000+</div>
+                        <div className="text-gray-500 text-xs sm:text-sm">High-Quality Products</div>
                     </div>
 
                     <div className="flex flex-col">
-                        <div className=" text-5xl font-satoshi_bold ">30,000+</div>
-                        <div className="text-gray-500">Happy Customer</div>
+                        <div className="text-2xl sm:text-5xl font-satoshi_bold ">30,000+</div>
+                        <div className="text-gray-500 text-xs sm:text-sm">Happy Customer</div>
                     </div>
                 </div>
             </div>
