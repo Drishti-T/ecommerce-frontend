6719b2f91c41043752e3792b8468e1d467268cee
Drishti-T <tanwardrishti20@gmail.com>
2025-04-15 16:18:12 +0530

    added StayUpToDate

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -5,6 +5,7 @@ import { Heading } from "../Components/Heading"
 import { HeroSection } from "../Components/HeroSection"
 import { NavBar } from "../Components/NavBar"
 import { ProductCard } from "../Components/ProductCard"
+import { StayUpToDate } from "../Components/StayUpToDate.jsx"
 import products from "../dummyData/products.js"
 export const Homepage = () => {
 
@@ -15,8 +16,7 @@ export const Homepage = () => {
         <Brands></Brands>
         <Heading text={"NEW ARRIVALS"}></Heading>
         <ProductCard products={products}></ProductCard>
+        <StayUpToDate></StayUpToDate>
         <BottomBar></BottomBar>
     </div>
 }
-
-
